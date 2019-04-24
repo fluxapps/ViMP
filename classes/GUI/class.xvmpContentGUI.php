ffccbff8d8c2fb61ced51f281221e0f1c42edd9f
Theodor Truffer <tt@studer-raimann.ch>
2019-04-24 10:57:12 +0200

    PLVIMP-39 (Transcoding is shown on deleted videos)

--- classes/GUI/class.xvmpContentGUI.php
+++ classes/GUI/class.xvmpContentGUI.php
@@ -78,13 +78,14 @@ class xvmpContentGUI extends xvmpGUI {
 			$tpl->setVariable('TITLE', $video->getTitle());
 			$tpl->setVariable('DESCRIPTION', strip_tags($video->getDescription(50)));
 
-			if ($video->getStatus() !== 'legal') {
-				$tpl->setCurrentBlock('info_transcoding');
-				$tpl->setVariable('INFO_TRANSCODING', $this->pl->txt('info_transcoding_short'));
-				$tpl->parseCurrentBlock();
-			}
-
 			if (!$video instanceof xvmpDeletedMedium) {
+
+				if ($video->getStatus() !== 'legal') {
+					$tpl->setCurrentBlock('info_transcoding');
+					$tpl->setVariable('INFO_TRANSCODING', $this->pl->txt('info_transcoding_short'));
+					$tpl->parseCurrentBlock();
+				}
+
 				$tpl->setVariable('LABEL_TITLE', $this->pl->txt( xvmpMedium::F_TITLE) . ':');
 				$tpl->setVariable('LABEL_DESCRIPTION', $this->pl->txt(xvmpMedium::F_DESCRIPTION) . ':');
 				$tpl->setVariable('LABEL_DURATION', $this->pl->txt(xvmpMedium::F_DURATION) . ':');
