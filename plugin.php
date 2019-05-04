87c219928ac3db92701255c0d9f45a47bfa70668
mstuder <ms@studer-raimann.ch>
2019-05-04 12:40:42 +0200

    set max-version to 5.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'xvmp';
 $version = '1.0.4';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'support@studer-raimann.ch';
 $learning_progress = true;
