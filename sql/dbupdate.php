65e848e2e376834a2672345b10f644ee0aca9f93
Sophie Pfister <sophie@fluxlabs.ch>
2021-08-30 14:29:30 +0200

    Fixing dbupdate #10

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -117,11 +117,13 @@ xvmpConf::set(xvmpConf::F_DEFAULT_PUBLICATION, 2);
 <#10>
 <?php
 $form_fields = [];
+if(!empty(xvmpConf::getConfig(xvmpConf::F_FORM_FIELDS))) {
 foreach (xvmpConf::getConfig(xvmpConf::F_FORM_FIELDS) as $field) {
     $field[xvmpConf::F_FORM_FIELD_SHOW_IN_PLAYER] = 1;
     $form_fields[] = $field;
 }
 if (!empty($form_fields)) {
     xvmpConf::set(xvmpConf::F_FORM_FIELDS, $form_fields);
+}
 }
 ?>
